f6bfd2e91132faf1d3b335757af000a10669efc1
Ben <benschenkenberger>
2017-03-31 15:06:19 -0700

    pass spec 3: add email

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -5,10 +5,12 @@ import org.sql2o.*;
 public class Stylist {
   private String name;
   private String phone;
+  private String email;
 
-  public Stylist(String name, String phone) {
+  public Stylist(String name, String phone, String email) {
     this.name = name;
     this.phone = phone;
+    this.email = email;
 
   }
 
@@ -18,5 +20,9 @@ public class Stylist {
 
   public String getPhone() {
     return phone;
+  }
+
+  public String getEmail() {
+    return email;
   }
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -10,20 +10,26 @@ public class StylistTest {
 
   @Test
   public void stylist_objectInstantiatesCorrectly_true() {
-    Stylist newStylist = new Stylist("Georgio Ramirez", "503-871-2356");
+    Stylist newStylist = new Stylist("Georgio Ramirez", "503-871-2356", "georgio.ramirezstylist@gmail.com");
     assertEquals(true, newStylist instanceof Stylist);
   }
 
   @Test
   public void stylist_instantiatesWithNameCorrectly() {
-    Stylist newStylist = new Stylist("Georgio Ramirez", "503-871-2356");
+    Stylist newStylist = new Stylist("Georgio Ramirez", "503-871-2356", "georgio.ramirezstylist@gmail.com");
     assertEquals("Georgio Ramirez", newStylist.getName());
   }
 
   @Test
   public void stylist_instantiatesWithPhoneNumberCorrectly() {
-    Stylist newStylist = new Stylist("Georgio Ramirez", "503-871-2356");
+    Stylist newStylist = new Stylist("Georgio Ramirez", "503-871-2356", "georgio.ramirezstylist@gmail.com");
     assertEquals("503-871-2356", newStylist.getPhone());
+  }
+
+  @Test
+  public void stylist_instantiatesWithEmailCorrectly() {
+    Stylist newStylist = new Stylist("Georgio Ramirez", "503-871-2356", "georgio.ramirezstylist@gmail.com");
+    assertEquals("georgio.ramirezstylist@gmail.com", newStylist.getEmail());
   }
 
 }
