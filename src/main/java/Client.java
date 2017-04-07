ad0b1f7438fa250f22374043378e34eb80b71479
Ben <benschenkenberger@gmail.com>
2017-04-07 10:01:51 -0700

    pass test: save adds client to database. Add equals override, save, and all methods to client

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,4 +1,4 @@
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import org.sql2o.*;
@@ -7,32 +7,85 @@ public class Client {
   private String name;
   private String phone;
   private String email;
-  private LocalDateTime created_at;
-  private int category_id;
+  private Timestamp created_at;
+  private int id;
+  private int stylist_id;
 
-  public Client(String name, String phone, String email) {
+  public Client(String name, String phone, String email, int stylist_id) {
     this.name = name;
     this.phone = phone;
     this.email = email;
-    created_at = LocalDateTime.now();
-    this.category_id = category_id;
   }
 
   public String getName() {
     return name;
   }
 
-  public String getPhone() {
-    return phone;
-  }
-
   public String getEmail() {
     return email;
   }
 
-  public LocalDateTime getCreatedAt() {
+  public Timestamp getCreatedAt() {
     return created_at;
   }
 
+  public int getStylistId() {
+    return stylist_id;
+  }
+
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO clients(name, email, created_at, phone, stylist_id) VALUES (:name, :email, now(), :phone, :stylist_id);";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("email", this.email)
+        .addParameter("stylist_id", this.stylist_id)
+        .addParameter("phone", this.phone)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherClient) {
+    if (!(otherClient instanceof Client)) {
+      return false;
+    } else {
+      Client newClient = (Client) otherClient;
+      return  this.getName().equals(newClient.getName()) &&
+              this.getEmail().equals(newClient.getEmail()) &&
+              this.getStylistId() == newClient.getStylistId() &&
+              this.getPhone().equals(newClient.getPhone()) &&
+              this.getId() == newClient.getId();
+    }
+  }
+
+  public static List<Client> all() {
+    String sql = "SELECT * FROM clients";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql)
+      .executeAndFetch(Client.class);
+    }
+  }
+  //
+  // public static Client find(int id) {
+  //   try(Connection con = DB.sql2o.open()) {
+  //     String sql = "SELECT * FROM clients where id=:id";
+  //     Client stylist = con.createQuery(sql)
+  //       .addParameter("id", id)
+  //       .executeAndFetchFirst(Client.class);
+  //     return stylist;
+  //   }
+  // }
 
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -10,31 +10,51 @@ public class ClientTest {
 
   @Test
   public void client_objectInstantiatesCorrectly_true() {
-    Client newClient = new Client("Bob Fredrickson", "971-275-8543", "bobfredrickson@gmail.com");
+    Client newClient = new Client("Bob Fredrickson", "971-275-8543", "bobfredrickson@gmail.com", 1);
     assertEquals(true, newClient instanceof Client);
   }
 
   @Test
   public void client_instantiatesWithNameCorrectly() {
-    Client newClient = new Client("Bob Fredrickson", "971-275-8543", "bobfredrickson@gmail.com");
+    Client newClient = new Client("Bob Fredrickson", "971-275-8543", "bobfredrickson@gmail.com", 1);
     assertEquals("Bob Fredrickson", newClient.getName());
   }
 
   @Test
   public void client_instantiatesWithPhoneCorrectly() {
-    Client newClient = new Client("Bob Fredrickson", "971-275-8543", "bobfredrickson@gmail.com");
+    Client newClient = new Client("Bob Fredrickson", "971-275-8543", "bobfredrickson@gmail.com", 1);
     assertEquals("971-275-8543", newClient.getPhone());
   }
 
   @Test
   public void client_instantiatesWithEmailCorrectly() {
-    Client newClient = new Client("Bob Fredrickson", "971-275-8543", "bobfredrickson@gmail.com");
+    Client newClient = new Client("Bob Fredrickson", "971-275-8543", "bobfredrickson@gmail.com", 1);
     assertEquals("bobfredrickson@gmail.com", newClient.getEmail());
   }
 
+
   @Test
-  public void client_instantiatesWithTimestampCorrectly() {
-    Client newClient = new Client("Bob Fredrickson", "971-275-8543", "bobfredrickson@gmail.com");
-    assertEquals(LocalDateTime.now().getDayOfWeek(), newClient.getCreatedAt().getDayOfWeek());
-  }
+ public void save_SavesAClientIntoDatabase_true() {
+   Client newClient = new Client("Bob Fredrickson", "971-275-8543", "bobfredrickson@gmail.com", 1);
+   newClient.save();
+   assertTrue(Client.all().get(0).equals(newClient));
+ }
+
+  // @Test
+  // public void all_returnsAllInstancesOfClient_true() {
+  //   Client newClient = new Client("Bob Fredrickson", "971-275-8543", "bobfredrickson@gmail.com", 1);
+  //   assertEquals("bobfredrickson@gmail.com", newClient.getEmail());
+  //   Client secondClient = new Client("Buy groceries", 1);
+  //   secondClient.save();
+  //   assertEquals(true, Client.all().get(0).equals(firstClient));
+  //   assertEquals(true, Client.all().get(1).equals(secondClient));
+  // }
+
+
+
+  // @Test
+  // public void client_instantiatesWithTimestampCorrectly() {
+  //   Client newClient = new Client("Bob Fredrickson", "971-275-8543", "bobfredrickson@gmail.com", 1);
+  //   assertEquals(DateFormat.getDateTimeInstance().format());
+  // }
 }
